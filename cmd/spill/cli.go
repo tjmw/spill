1c440364ae98610b7c4dba490a80ef0175400d40
Tom Wey <tjmwey@gmail.com>
2018-12-29 23:12:48 +0000

    Split Cli out into its own file

---
+++ cmd/spill/cli.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli"
+)
+
+func Cli() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Spill"
+	app.Usage = "Sync secure notes with 1Password"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "name",
+			Value: ".env.staging",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "pull",
+			Usage: "Fetch secure note from 1Password",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				fmt.Println("Pull from 1Password")
+				return nil
+			},
+		},
+		{
+			Name:  "push",
+			Usage: "Push secure note to 1Password",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				fmt.Println("Push to 1Password")
+				return nil
+			},
+		},
+	}
+
+	return app
+}
--- cmd/spill/spill.go
+++ cmd/spill/spill.go
@@ -1,45 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-
-	"github.com/urfave/cli"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "Spill"
-	app.Usage = "Sync secure notes with 1Password"
-
-	flags := []cli.Flag{
-		cli.StringFlag{
-			Name:  "name",
-			Value: ".env.staging",
-		},
-	}
-
-	app.Commands = []cli.Command{
-		{
-			Name:  "pull",
-			Usage: "Fetch secure note from 1Password",
-			Flags: flags,
-			Action: func(c *cli.Context) error {
-				fmt.Println("Pull from 1Password")
-				return nil
-			},
-		},
-		{
-			Name:  "push",
-			Usage: "Push secure note to 1Password",
-			Flags: flags,
-			Action: func(c *cli.Context) error {
-				fmt.Println("Push to 1Password")
-				return nil
-			},
-		},
-	}
+	app := Cli()
 
 	err := app.Run(os.Args)
 	if err != nil {
