6f3b9b7742c7930d71a6e7f3acf5044b469f49eb
Tom Wey <tjmwey@gmail.com>
2018-12-29 22:39:30 +0000

    Add a couple of empty CLI actions

--- cmd/spill/spill.go
+++ cmd/spill/spill.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -8,7 +9,39 @@ import (
 )
 
 func main() {
-	err := cli.NewApp().Run(os.Args)
+	app := cli.NewApp()
+	app.Name = "Spill"
+	app.Usage = "Sync secure notes with 1Password"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "name",
+			Value: ".env.staging",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "pull",
+			Usage: "Fetch secure note from 1Password",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				fmt.Println("Pull from 1Password")
+				return nil
+			},
+		},
+		{
+			Name:  "push",
+			Usage: "Push secure note to 1Password",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				fmt.Println("Push to 1Password")
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
